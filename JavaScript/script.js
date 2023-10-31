d29aa5b78a8615277011286b88611e5b7ae316e8
gadqxq <yoda360mlg@gmail.com>
2023-10-31 01:50:51 +0100

    Add a function to display tasks
    Create a function to display tasks in the task list
    
    #7

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -12,9 +12,44 @@ function addTask() {
     if (taskText) {
         tasks.push({
             text: taskText, // Legger til oppgavetekst
-            completed: false // Setter fullført-status til falsk som standard
+            completed: false // Setter fullført-status til false som standard
         });
         taskInput.value = ""; // Tømmer inndatafeltet
         displayTasks(); // Viser oppgavene på nytt
     }
+}
+
+// Funksjon for å vise oppgavene i oppgavelisten
+function displayTasks() {
+    taskList.innerHTML = ""; // Tømmer oppgavelisten før oppdatering
+    tasks.forEach((task, index) => {
+        const taskBox = document.createElement("div"); // Oppretter en div for hver oppgave
+        taskBox.className = "task-box"; // Legger til CSS-klasse for oppgaveboksen
+        const taskItem = document.createElement("div"); // Oppretter en div for oppgaveelementet
+        taskItem.className = "task-item"; // Legger til CSS-klasse for oppgaveelementet
+        taskItem.innerHTML = `
+            <input type="checkbox" class="checkbox" id="task${index}" ${task.completed ? "checked" : ""}>
+            <label for="task${index}" class="${task.completed ? "completed-task" : ""}">${task.text}</label>
+            <img class="delete-button" alt="x" src="./img/xmark-solid.svg" data-index="${index}">
+        `; // Legger til HTML-innhold for oppgaveelementet
+        taskBox.appendChild(taskItem); // Legger oppgaveelementet til oppgaveboksen
+        taskList.appendChild(taskBox); // Legger oppgaveboksen til oppgavelisten
+        // Legger til hendelseslyttere for sjekkboksen, sletteknappen og oppgaveelementet
+        const checkbox = taskItem.querySelector(`#task${index}`);
+        checkbox.addEventListener("change", () => {
+            task.completed = checkbox.checked; // Oppdaterer fullføringsstatus når sjekkboksen endres
+            updateTaskStatus(); // Oppdaterer statusmeldingen
+        });
+        const deleteButton = taskItem.querySelector(".delete-button");
+        deleteButton.addEventListener("click", (event) => {
+            event.stopPropagation();
+            const indexToDelete = event.target.getAttribute("data-index");
+            deleteTask(indexToDelete); // Sletter oppgaven når sletteknappen klikkes
+        });
+        taskItem.addEventListener("click", () => {
+            task.completed = !task.completed; // Bytter fullføringsstatus når oppgaveelementet klikkes
+            updateTaskStatus(); // Oppdaterer statusmeldingen
+        });
+    });
+    updateTaskStatus(); // Oppdaterer status for fullførte oppgaver
 }
